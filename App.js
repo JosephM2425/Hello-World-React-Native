c2d84ed3155eecf587c9070b0b1b21d30450d8cd
Joseph Murillo <josephmurillo2425@gmail.com>
2021-04-14 00:31:41 -0600

    First Commit | Learning React-Native

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Componentes de react  native
+import {Text, View, StyleSheet, Image, Button, Alert, TouchableOpacity} from 'react-native';
+// import { WebView } from 'react-native-webview';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Aprendiendo React Native Basico</Text>
+      <Image 
+        source={{uri: 'https://cdn.vox-cdn.com/thumbor/H3kPpG-J5Z3rMpc6h60zxyJAdoQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19396981/shutterstock_1413240503.jpg'}}
+        style={styles.img} 
+      />
+      {/* // <WebView source={{ uri: 'URL DE LA PAGINA' }} /> */}
+
+      {/* <Button 
+        onPress= {()=> { Alert.alert('Hola me presionaron') }}
+        title="Hola soy un boton" 
+        color="#007AFF"
+      /> */}
+
+      <TouchableOpacity
+        onPress= {()=> { Alert.alert('Hola me presionaron') }}
+        style={styles.btn}
+      >
+        <Text style={styles.btntext}>Presioname!</Text>
+      </TouchableOpacity>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#832652'
   },
-});
+  title: { fontSize: 20, color: '#fff'},
+  img: { height: 75, width: 100, borderRadius: 20},
+  btn: { 
+    backgroundColor: 'mediumseagreen',
+    padding: 7,
+    marginTop: 10
+  }, 
+
+  btntext: {
+    color: '#fff',
+    fontSize: 20
+  }
+
+
+})
+
+export default App;
